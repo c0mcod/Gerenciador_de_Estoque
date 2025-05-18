79165419c96580f43a7a11fc145ab983e2a9e0a8
comcod <netosilvaneto6@gmail.com>
2025-05-18 16:06:18 -0300

    models fornecedores e produtos

---
+++ src/br/com/estoque/produto/model/Produto.java
@@ -0,0 +1,50 @@
+package br.com.estoque.produto.model;
+
+public class Produto {
+	private int id;
+	private int id_fornecedor;
+	private String nome;
+	private String preco;
+	private int quantidade;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getId_fornecedor() {
+		return id_fornecedor;
+	}
+
+	public void setId_fornecedor(int id_fornecedor) {
+		this.id_fornecedor = id_fornecedor;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getPreco() {
+		return preco;
+	}
+
+	public void setPreco(String preco) {
+		this.preco = preco;
+	}
+
+	public int getQuantidade() {
+		return quantidade;
+	}
+
+	public void setQuantidade(int quantidade) {
+		this.quantidade = quantidade;
+	}
+
+}
